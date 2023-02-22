a448f4df1522d90251a0dd6f61a79332a2ddc4e6
ekfman <ekfirstman@gmail.com>
2023-02-22 10:52:58 -0800

    styling

--- e-journal-frontend/src/components/App.js
+++ e-journal-frontend/src/components/App.js
@@ -1,4 +1,4 @@
-// import "./App.css";
+import "../style/App.css"
 import { Link, Routes, Route, useNavigate } from "react-router-dom";
 import CreateEntry from "./CreateEntry";
 import { useEffect, useState } from "react";
