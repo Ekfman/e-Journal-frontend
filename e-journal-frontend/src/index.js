5460efb76d8c29e35cc54e4cab83a2ddd7e29a45
ekfman <ekfirstman@gmail.com>
2023-02-22 10:30:32 -0800

    style folder

---
+++ e-journal-frontend/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { render } from 'react-dom'
+import  App  from './components/App';
+
+const root = document.getElementById('root');
+render(
+    <React.StrictMode>
+      <BrowserRouter><App /></BrowserRouter>
+    </React.StrictMode>,
+    root
+  );
